a57b924fd2487b9c2a8743d98b14b15ba85ae6ce
yuting813 <yutinghu813@gmail.com>
2023-05-29 16:45:55 +0800

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+
+module.exports = {
+	images: {
+		domains: ['image.tmdb.org'],
+	},
+};
+
+// module.exports = nextConfig;
+// const nextConfig = {
+// 	reactStrictMode: true,
+// };
+
+// module.exports = {
+// 	images: {
+// 	  remotePatterns: [
+// 		{
+// 		  protocol: 'https',
+// 		  hostname: 'image.tmdb.org'
+// 		},
+// 	  ],
+// 	},
+//   }
